51ae1367dcfee285f7a2aab9ed262e5d03f6c7c5
Timothy Juntunen <21698171+darwinIcesurfer@users.noreply.github.com>
2024-03-21 16:05:22 -0500

    Created launch and tasks for VSCode to launch a console project for debugging use

---
+++ XPlaneConnector/XPlaneCommandLineStart/ConsoleDebug.cs
@@ -0,0 +1,17 @@
+﻿using XPlaneConnector;
+
+namespace XPlaneCommandLineStart
+{
+    public static class ConsoleDebug
+    {
+        public static void Main(string[] args)
+        {
+            var connector = new XPlaneConnector.XPlaneConnector(); // Default IP 127.0.0.1 Port 49000
+            connector.Start();
+        
+
+            // Additional code here...
+        }
+    }
+}
+
